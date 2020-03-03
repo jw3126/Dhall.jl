9e7f69e4c9de68fd28f1077d0b56fe07ebd0325e
Jan Weidner <jw3126@gmail.com>
2020-03-03 21:43:26 +0100

    fix

--- src/Dhall.jl
+++ src/Dhall.jl
@@ -2,16 +2,11 @@ module Dhall
 
 using Pkg.Artifacts: @artifact_str
 
-const PATHS = Dict{String, String}()
-function __init__()
-
-    d = Dict(
+const PATHS = Dict(
         "dhall"         => joinpath(artifact"dhall", "bin", "dhall"),
         "dhall-to-json" => joinpath(artifact"dhall-json", "bin", "dhall-to-json"),
         "json-to-dhall" => joinpath(artifact"dhall-json", "bin", "json-to-dhall"),
         "dhall-to-yaml" => joinpath(artifact"dhall-json", "bin", "dhall-to-yaml"),
-    )
-    merge!(PATHS, d)
-end
+       )
 
 end#module
